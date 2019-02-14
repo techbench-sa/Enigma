9b4b69ebe62c484874bd1290df5b7cd4a6562929
Mohammed Alobaidi <Mohalobaidi17@gmail.com>
2019-02-15 01:54:01 +0300

    removed all old files

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-const components = require.context('./', true, /\.vue$/)
-
-export default Vue => {
-components.keys()
-  .filter(d => d.match(/\.\/(\w+)\.vue/g))
-  .map(d => components(d).default)
-  .forEach(component => Vue.component(component.name, component))
-}
--- src/views/Challenge.vue
+++
@@ -1,8 +0,0 @@
-<template lang="pug">
-#Challenge(style="height: 100%")
-  .container(style="padding: 72px 0; height: 100%")
-    Editor
-</template>
-
-<script>
-</script>
