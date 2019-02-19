486e21cdad026b99eef86534784ea1713495a530
Mohammed Alobaidi <Mohalobaidi17@gmail.com>
2019-02-19 13:37:36 +0300

    Send Error message if submission is empty

--- templates/java.js
+++ templates/java.js
@@ -18,18 +18,23 @@ const generateSubmission = (challenge, submission) => {
 const { method_name, method_type } = challenge
 const params = JSON.parse(challenge.parameters)
 const tests = JSON.parse(challenge.tests)
-return `
-import java.util.Arrays;
-import java.io.OutputStream;
-import java.io.PrintStream;
-class Challenge_${challenge.id} {
 console.log(check(submission))
+return `class Challenge_${challenge.id} {
+
+  public static ${method_type} ${method_name} (${params.map(param => `${type(param.type)} ${param.name}`).join(', ')}) {
+${
+  check(submission) ? submission
+  : `isEmpty = true; return ${method_type == 'String' ? '""' : method_type == 'Boolean' ? 'false' : '0'};`
+}
+  }
+
+  public static boolean isEmpty = false;
 
   public static void main (String[] args) {
 
       // Preventing user from printing
-      PrintStream originalStream = System.out;
-      PrintStream dummyStream = new PrintStream(new OutputStream () {
+      java.io.PrintStream originalStream = System.out;
+      java.io.PrintStream dummyStream = new java.io.PrintStream(new java.io.OutputStream () {
           public void write(int b) {
               // NO-OP
               // deduct point if user tried to print ;)
@@ -42,18 +47,24 @@ console.log(check(submission))
       
       for (int i = 0; i < results.length; i++) {
           System.setOut(dummyStream);
-          ${type(method_type)} res = ${method_name}(${params.map((_, i) => `args${i}[i]`).join(', ')});
-          results[i] = ${method_type == 'String' ? 'res.equals(outputs[i])' : 'res == outputs[i]'};
-          System.setOut(originalStream);
-          System.out.println("{\\"type\\":\\"test\\",\\"payload\\":{\\"test\\":"+i+",\\"value\\":"+results[i]+"}}");
+          try {
+            ${type(method_type)} res = ${method_name}(${params.map((_, i) => `args${i}[i]`).join(', ')});
+            results[i] = ${method_type == 'String' ? 'res.equals(outputs[i])' : 'res == outputs[i]'};
+            System.setOut(originalStream);
+            if (isEmpty) {
+              if (i == 0)
+                System.out.println("{\\"error\\":\\"test\\",\\"payload\\":{\\"message\\":\\"You didn\'t write anything!\\"}}");
+              results[i] = false;
+            } 
+            System.out.println("{\\"type\\":\\"test\\",\\"payload\\":{\\"test\\":"+i+",\\"value\\":"+results[i]+"}}");
+          } catch (Exception e) {
+            System.setOut(originalStream);
+            System.out.println("{\\"error\\":\\"test\\",\\"payload\\":{\\"message\\":\\""+e.toString()+"\\"}}");          
+          }
+
       }
   }
 
-  public static ${method_type} ${method_name} (${params.map(param => `${type(param.type)} ${param.name}`).join(', ')}) {
-
-      ${submission || ''}
-
-  }
 
 }
 `
@@ -64,6 +75,12 @@ const getSignature = challenge => {
   const { method_name, method_type } = challenge
   const params = JSON.parse(challenge.parameters)
   return `public static ${type(method_type)} ${method_name} (${params.map(param => `${type(param.type)} ${param.name}`).join(', ')})`
+}
+
+const check = submission => {
+  if (submission.replace(/\/\/.*\n/g, '').replace(/\/\*(.|\n)*?\*\//g, '').trim() === '')
+    return false
+  return true
 }
 
 module.exports = { generateSubmission, getSignature }
