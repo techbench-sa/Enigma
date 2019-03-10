adac9abb3ba5a3a0ac9e51fc6c5b0acadebc4be2
Hadi Albinsaad <ihadabs@gmail.com>
2019-03-10 09:23:26 +0300

    Tests

--- app/database.js
+++ app/database.js
@@ -45,6 +45,7 @@ pool.connect(err => {
 
 module.exports = {
   getUserByUsername: username => {
+    console.log("hey hadi")
     return pool.query(GET_USER_BY_USERNAME, [username]).then(res => {
       return { ...res.rows[0] }
     })
