f5aa4d80a85aa0bc371c9cab7b478ba33b5a5b05
Mohammed Alobaidi <Mohalobaidi17@gmail.com>
2019-03-10 23:27:10 +0300

    Update Users.vue

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -6,10 +6,12 @@
     table
       colgroup
         col(width="0%")
-        col(width="25%")
-        col(width="25%")
-        col(width="20%")
-        col(width="20%")
+        col(width="15%")
+        col(width="15%")
+        col(width="15%")
+        col(width="15%")
+        col(width="15%")
+        col(width="15%")
         col(width="10%")
       thead
         tr
